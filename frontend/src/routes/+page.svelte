e7ede493a7dc9ce2ebb939bbc2669aac3d1139e9
origin-unknown <ich.hier.wer.dort@gmx.de>
2024-11-05 12:51:27 +0100

    updated to Svelte5

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
-	let rand = -1;
+	import { onMount } from 'svelte';
+
+	let rand = $state(-1);
 
 	function getRand() {
 		fetch('/api/rand')
 			.then(d => d.text())
 			.then(d => (rand = d));
 	}
+
+	onMount(getRand)
 </script>
 
 <h1>Your number is {rand}!</h1>
-<button on:click={getRand}>Get a random number</button>
+<button onclick={getRand}>Get a random number</button>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api/rand': 'http://127.0.0.1:5000',
-        }
-    },
+	server: {
+		proxy: {
+			'/api/rand': 'http://127.0.0.1:5000',
+		}
+	},
 	plugins: [sveltekit()]
 });
